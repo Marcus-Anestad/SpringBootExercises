6f39ffd8aed2d8bf199c349385b831d34681bd88
Marcus-Anestad <tuj23850@temple.edu>
2022-04-07 13:24:50 -0400

    Initialized Exercise103

---
+++ Exercise103/src/test/java/com/example/exercise103/Exercise103ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercise103;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise103ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
