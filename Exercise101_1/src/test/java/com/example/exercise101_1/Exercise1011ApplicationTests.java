16b781defc29fa22b5a1399c45353239d8529db1
Marcus-Anestad <tuj23850@temple.edu>
2022-04-07 12:48:30 -0400

    Created Exercise101_1
    Created a SpringBoot project in InteliJ

---
+++ Exercise101_1/src/test/java/com/example/exercise101_1/Exercise1011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercise101_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise1011ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
