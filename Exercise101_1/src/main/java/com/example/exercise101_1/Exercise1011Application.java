0095c1c58c818825673cb6326b3a38592d517802
Marcus-Anestad <tuj23850@temple.edu>
2022-04-07 12:54:10 -0400

    Completed Exercise101_1

--- Exercise101_1/src/main/java/com/example/exercise101_1/Exercise1011Application.java
+++ Exercise101_1/src/main/java/com/example/exercise101_1/Exercise1011Application.java
@@ -2,12 +2,18 @@ package com.example.exercise101_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Exercise1011Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Exercise1011Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String getHome(){
+        return "index";
     }
 
 }
